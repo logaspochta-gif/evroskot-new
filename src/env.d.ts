5956c5f03981f0f321a05f358e7172d1bdbf1132
Anton <logaspochta@gmail.com>
2026-05-12 17:25:55 +0700

    обратная форма и новости

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,3 +1,7 @@
 // src/env.d.ts
 declare var Swiper: any;
 declare var WOW: any;
+interface Window {
+  VK_ACCESS_TOKEN_FOR_DEV?: string;
+  openPostModal?: (ownerId: string, postId: string) => Promise<void>;
+}
